dc2ad28a0183ab6d73c1e7b4a07222fd92b2fbbd
pulkit6369 <131269479+pulkit6369@users.noreply.github.com>
2024-12-08 23:57:12 +0530

    navbar change

--- src/Components/Banner_wrapper/Banner_wrapper.jsx
+++ src/Components/Banner_wrapper/Banner_wrapper.jsx
@@ -1,30 +1,46 @@
 
 
-function Banner_wrapper() {
+function BannerWrapper() {
     return (
         <>
-            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
                 <div className="carousel-inner">
-                    <div className="carousel-item active" data-bs-interval="1000">
-                        {/* <img src="./images/Banner_wrapper/1.png" className="d-block w-100" alt="..." /> */}
-                        <img src="https://img.freepik.com/free-psd/college-template-design_23-2151655478.jpg?t=st=1733464956~exp=1733468556~hmac=d886a399666ca69a39f183d4b05c8e8ff63cc09feb4bfab8f8daacff5c50ba55&w=1380" className="d-block w-100" alt="..." />
+                    <div className="carousel-item active" data-bs-interval="3000">
+                        <img
+                            src="https://img.freepik.com/free-psd/college-template-design_23-2151655478.jpg?t=st=1733464956~exp=1733468556~hmac=d886a399666ca69a39f183d4b05c8e8ff63cc09feb4bfab8f8daacff5c50ba55&w=1380"
+                            className="d-block w-100"
+                            alt="College Template"
+                        />
                     </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src="https://img.freepik.com/free-psd/business-corporation-template-design_23-2151376449.jpg?t=st=1733464690~exp=1733468290~hmac=d841451bf9366bfa903dc76615e09f0dce13252d02e018e26f6bec781701f42a&w=1380" className="d-block w-100" alt="..." />
+                    <div className="carousel-item" data-bs-interval="3000">
+                        <img
+                            src="https://img.freepik.com/free-psd/business-corporation-template-design_23-2151376449.jpg?t=st=1733464690~exp=1733468290~hmac=d841451bf9366bfa903dc76615e09f0dce13252d02e018e26f6bec781701f42a&w=1380"
+                            className="d-block w-100"
+                            alt="Business Corporation Template"
+                        />
                     </div>
-                    
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="prev"
+                >
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="next"
+                >
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
         </>
-    )
+    );
 }
 
-export default Banner_wrapper
+export default BannerWrapper;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-
-import { Link, NavLink } from 'react-router-dom'
-import './Navbar.css'
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
+import Submenuu from './Submenu';
 
 function Navbar() {
     return (
@@ -8,38 +8,58 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg navbar-light m-3 bg-back">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
-                    <img src="./images/Logo/log.png" alt="Logo" className='img-fuild' />
+                        <img src="./images/Logo/log.png" alt="Logo" className='img-fluid' />
                     </Link>
-                    <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="fas fa-stream"></i>
+                    <button
+                        className="navbar-toggler shadow-none border-0"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i className="fas fa-stream"></i>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-4  mb-lg-0 menu-nav ">
-                            <li className="nav-item  my-sm-2">
-                                <NavLink className="nav-link mx-4 fs-5 program text-light px-5" to="/">Find Programs</NavLink>
+                        <ul className="navbar-nav ms-auto mb-4 mb-lg-0 menu-nav">
+                            <li className="nav-item dropdown my-sm-2">
+                                <NavLink
+                                    className="nav-link dropdown-toggle mx-4 fs-5 program text-light px-5"
+                                    to="#"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Find Programs
+                                </NavLink>
+                                <Submenuu />
                             </li>
                             <li className="nav-item uni my-sm-2">
-                                <NavLink className="top mx-4 fs-5 fw-bold px-3" to="/top-universities">Top Universities</NavLink>
-                            </li>
-                            <li className="nav-item my-sm-2">
-                                <NavLink className="nav-link fs-5 mx-4 expert px-3" to="/expert-advice">✅ Expert Advice</NavLink>
-                            </li>
-                            <li className="nav-item my-sm-2 fs-5">
-                                <NavLink className="nav-link num mx-4 px-3 " to="/">
-                                <span className='fw-bold rounded-pill d-flex '>HelpLine</span>
-                                9785-800-008
+                                <NavLink className="top mx-4 fs-5 fw-bold px-3" to="/top-universities">
+                                    Top Universities
                                 </NavLink>
                             </li>
-
+                            <li className="nav-item my-sm-2">
+                                <NavLink className="nav-link fs-5 mx-4 expert px-3" to="/expert-advice">
+                                    ✅ Expert Advice
+                                </NavLink>
+                            </li>
+                            <li className="nav-item my-sm-2 fs-5">
+                                <NavLink className="nav-link num mx-4 px-3" to="/">
+                                    <span className='fw-bold rounded-pill d-flex'>HelpLine</span>
+                                    9785-800-008
+                                </NavLink>
+                            </li>
                         </ul>
-                        <form className="d-flex ps-5 ">
+                        <form className="d-flex ps-5">
                             <button className="btn btn-outline-danger fs-5 px-3 py-2" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
